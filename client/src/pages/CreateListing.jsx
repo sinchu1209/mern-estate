a38c2f26c938b9439864a811eeff1f17d19fa21e
sinchu1209 <148935200+sinchu1209@users.noreply.github.com>
2024-07-04 10:20:15 +0530

    complete listing page

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -253,7 +253,9 @@ const handleRemoveImage = (index) => {
               />
               <div className='flex flex-col items-center'>
                 <p>Regular price</p>
-                <span className='text-xs'>(₹ / month)</span>
+                {formData.type === 'rent' && (
+                  <span className='text-xs'>(₹ / month)</span>
+                )}
               </div>
             </div>
             {formData.offer && (
@@ -270,7 +272,9 @@ const handleRemoveImage = (index) => {
                 />
                 <div className='flex flex-col items-center'>
                   <p>Discounted price</p>
-                  <span className='text-xs'>(₹ / month)</span>
+                  {formData.type === 'rent' && (
+                    <span className='text-xs'>(₹ / month)</span>
+                  )}
                 </div>
             </div>
             )}
